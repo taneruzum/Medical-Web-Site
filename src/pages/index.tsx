f7a80bc3b497701f4d20ce5a7ac9d58406d6fc52
Uğur Akçay <akcayugur01@gmail.com>
2023-07-08 18:47:19 +0300

    aa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {Navigation } from 'swiper/modules';
 import 'swiper/css/navigation';
 import NavBar from '../app/Components/NavBar/NavBar';
 import "../pages/index.css"
@@ -8,25 +8,38 @@ import "../pages/index.css"
 export default function Home() {
   return (
     <div>
-      <div className='body'>
-        <NavBar/> 
-        <div className="Sliderr">
-                ugur11231231231
-              aaaa
+      <div className='mainPageContainer'>
+        <NavBar />
+        <div className='sliderContainer'>
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+          >
+            <SwiperSlide>
+              <img src="/images/photo1.webp" alt="" />
+            </SwiperSlide>
+          </Swiper>
         </div>
-        
-          
-        
-    
-      </div>
-        
-        
-    </div>
-      
-      
-           
+        <div style={{ width: "100%", textAlign: "center", marginTop: "2rem", marginBottom: "2rem", height: "50px" }}>
+          deneme
+        </div>
+        <div style={{ width: "100%", textAlign: "center", marginTop: "2rem", marginBottom: "2rem", height: "50px" }}>
+          deneme2
+        </div>
+        <div style={{ width: "100%", textAlign: "center", marginTop: "2rem", marginBottom: "2rem", height: "50px" }}>
+          deneme3
+        </div>
+        <div style={{ width: "100%", textAlign: "center", marginTop: "2rem", marginBottom: "2rem", height: "50px" }}>
+          deneme4
+        </div>
 
-  
-    
+      </div>
+    </div>
+
+
+
+
+
+
   )
 }
